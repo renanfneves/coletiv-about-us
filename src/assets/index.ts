6adf1f6707a4dd0e1c67782ec365c75b4a7ccdcd
Renan Neves <renanfneves@gmail.com>
2020-12-15 15:01:57 -0300

    [feature]: Add assets to project

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+export { default as Logo } from './logo';
+export { default as CompleteLogo } from './completeLogo';
+export { default as LinkedIn } from './linkedIn';
+export { default as Twitter } from './twitter';
+export { default as Facebook } from './facebook';
+export { default as Instagram } from './instagram';
+export { default as Dribbble } from './dribbble';
+export { default as DownArrow } from './downArrow';
+
+export { default as PastImg } from './coletiv-studio-past.jpg';
+export { default as GrowthImg } from './coletiv-studio-starting-team.jpg';
+export { default as TeamImg } from './coletiv-studio-team-gathering.jpg';
+export { default as OfficeImg } from './coletiv-studio-office.jpg';
+export { default as PresentTeamImg } from './coletiv-studio-present-team.jpg';
+export { default as FutureImg } from './coletiv-studio-future.jpg';
